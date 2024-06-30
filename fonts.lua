aa8c395d33e42ee485ad49acb6ac91e8f5b1347d
greg <grzegorz.kozub@gmail.com>
2024-06-30 10:56:33 +0200

    dim font not ugly

--- fonts.lua
+++ fonts.lua
@@ -1,9 +1,19 @@
 local M = {}
 
 function M.config(wezterm, config)
-  config.font = wezterm.font {
+  local font = {
     family = 'Cascadia Code NF',
     harfbuzz_features = { 'calt = 1', 'ss01 = 1' },
+  }
+  config.font = wezterm.font(font)
+
+  font.weight = 'DemiLight'
+  config.font_rules = {
+    {
+      intensity = 'Half',
+      italic = false,
+      font = wezterm.font(font),
+    },
   }
 
   config.font_size = 14.0
