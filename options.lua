6df819cdaaa99781cdd65e85855bc7a95aa5f0b9
greg <grzegorz.kozub@gmail.com>
2026-03-29 11:28:42 +0200

    fix claude new lines with shift+enter

--- options.lua
+++ options.lua
@@ -4,6 +4,7 @@ function M.config(config)
   config.automatically_reload_config = false
   config.check_for_updates = false
   config.command_palette_rows = 16
+  config.enable_kitty_keyboard = true
 end
 
 return M
